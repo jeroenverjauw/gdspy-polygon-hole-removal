94ed2d9e52efebe3378a573361b17478f3f63e00
verjau76 <verjau76@imec.be>
2018-08-24 19:17:00 +0200

    wip

--- Main.py
+++ Main.py
@@ -5,7 +5,27 @@ from tkinter import filedialog
 gdsii = gdspy.GdsLibrary()
 gdspy.current_library = gdsii
 
-gdsii.read_gds("C:/Users/jeroen verjauw/Desktop/SWMAJ-MEC_HFSS_design.gds")
+gdsii.read_gds("C:/Users/verjau76/Desktop/SWMAJ-MEC_HFSS_design.gds")
+
+
+def invert_layer(cell,lyr):
+
+    mask = []
+    polygons = cell.get_polygons(by_spec=True)[lyr]
+    box_coord = cell.get_bounding_box()
+    bbox = gdspy.Rectangle(box_coord[0],box_coord[1])
+
+    for p in polygons:
+        mask.append(gdspy.Polygon(p))
+
+    result = ((gdspy.fast_boolean(bbox, mask, 'not',layer=lyr[0])))
+    if result is not None:
+        cell.remove_polygons(lambda pts, layer, datatype:layer == lyr[0])
+        cell.add(result)
+        return result.polygons
+
+
+
 
 def tessellate(cell, lyr=None):
     """
@@ -21,14 +41,13 @@ def tessellate(cell, lyr=None):
 
     mask = []
     if lyr is None:
-        layers = topcell.get_polygons(by_spec = True)
+        layers = cell.get_polygons(by_spec = True)
         for l in layers:
-            print('test:{}'.format(l))
-            tessellate(topcell,l)
+            tessellate(cell,l)
         return None
 
     polygons = cell.get_polygons(by_spec=True)[lyr]
-    box_coord = topcell.get_bounding_box()
+    box_coord = cell.get_bounding_box()
     bbox = gdspy.Rectangle(box_coord[0],box_coord[1])
 
     for p in polygons:
@@ -41,11 +60,23 @@ def tessellate(cell, lyr=None):
 
 
 for topcell in gdsii.top_level():
-    tessellate(topcell)
-    # layers = topcell.get_polygons(by_spec = True)
-    # for l in layers:
-    #     print(l)
-    #     tessellate(topcell,l)
+    # tessellate(topcell)
+    gdspy.LayoutViewer(cells = topcell, depth=len(topcell.get_dependencies(recursive = True)))
+    box_coord = topcell.get_bounding_box()
+    layers = topcell.get_polygons(by_spec = True)
+    for l in layers:
+        inv = invert_layer(topcell,l)
+        if (inv) is not None:
+            # print(box_coord[0][0],box_coord[1])
+            bbox = []
+            for p in inv:
+                bbox.append(gdspy.PolygonSet(inv).get_bounding_box())
+            # 1 coordinaat moet op de rand liggen
+            if any((coord[0][0] > box_coord[0][0] and coord[0][1] >  box_coord[0][1]) and (coord[1][0] < box_coord[1][0] and coord[1][1] <  box_coord[1][1]) for coord in bbox):
+                  print(l)
+            # else we need to test a few other things!
+
+
 
     gdspy.write_gds('test2.gds')
     gdspy.LayoutViewer(cells = topcell, depth=len(topcell.get_dependencies(recursive = True)))
